edc5c61aa138857fdcb9fd55cca979b0293f4c15
Pmobilee <muldamion@gmail.com>
2023-09-16 15:30:26 +0200

    fix

--- v_diffusion/train_utils.py
+++ v_diffusion/train_utils.py
@@ -358,7 +358,7 @@ class Trainer:
                             session.log({"fid (500)": fid})
                             print(fid)
 
-                            self.save_checkpoint(chkpt_path, epoch=str(e+1) + "_i")
+                            self.save_checkpoint(chkpt_path, epoch=str(e+1) + "_"+ {i})
                             self.model.train()
 
                         
