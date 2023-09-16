7ae88e3f5c89093a35b1263c068dde6e888555d6
Pmobilee <muldamion@gmail.com>
2023-09-16 16:37:14 +0200

    back

--- v_diffusion/train_utils.py
+++ v_diffusion/train_utils.py
@@ -182,8 +182,8 @@ class Trainer:
 
         
         # Combine losses with weights (hyperparameters)
-        alpha = 0.8  # weight for main loss
-        beta = 0.2  # weight for distillation loss
+        alpha = 0.9  # weight for main loss
+        beta = 0.1  # weight for distillation loss
         
         loss = alpha * main_loss + beta * distill_loss
         # loss = main_loss
