824de16b61cc0460bd8b0eb4b09b932abc1e28c0
Pmobilee <muldamion@gmail.com>
2023-09-18 19:41:03 +0200

    whoops

--- v_diffusion/train_utils.py
+++ v_diffusion/train_utils.py
@@ -181,7 +181,7 @@ class Trainer:
         B = x.shape[0]
         main_loss, distill_loss = self.train_distill_loss(x, y)
 
-        beta = torch.min(1, 15000 / i) * 0.2
+        beta = torch.min(1, 15000 / i+1) * 0.2
         # Combine losses with weights (hyperparameters)
         alpha = 0.8  # weight for main loss
         # beta = 0.025  # weight for distillation loss
