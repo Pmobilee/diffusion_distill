d0a63b642eb266e0b8731952ae5e42d67e7f10fc
Pmobilee <muldamion@gmail.com>
2023-09-20 18:58:38 +0200

    small edit

--- FID.py
+++ FID.py
@@ -23,7 +23,8 @@ def list_folders(directory_path):
 @errors.record
 def main(args):
     fid_dataframe = pd.DataFrame(columns=['dataset', 'checkpoint', 'fid'])
-    chkpts_dir = r"/run/media/damion/Data/Thesis BACKUP/distilled/"
+    # chkpts_dir = r"/run/media/damion/Data/Thesis BACKUP/distilled/"
+    chkpts_dir = r"./chkpts/"
     datasets = list_folders(chkpts_dir)
     
     for dataset in datasets:
